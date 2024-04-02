06e7a72a3d982ae84ed8e9390b5c3b70ee3afdd2
Illia <ilyaivasyk1@gmail.com>
2024-04-02 21:16:51 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,28 +1,21 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let personStatus;
+
+  if (isMarried) {
+    personStatus =
+      sex === 'f'
+        ? `${partnerName} is my husband`
+        : `${partnerName} is my wife`;
+  } else {
+    personStatus = 'I am not married';
+  }
+
   return (
     <div className="Person">
-      <div className="Person__name">My name is {person.name}</div>
-      {person.age && person.age !== null && (
-        <div className="Person__age">I am {person.age}</div>
-      )}
-      {person.sex === 'm' && person.sex !== null && (
-        <div className="Person__partner">
-          {person.isMarried
-            ? `${person.partnerName} is my wife`
-            : 'I am not married'}
-        </div>
-      )}
-      {person.sex === 'f' && person.sex !== null && (
-        <div className="Person__partner">
-          {person.isMarried
-            ? `${person.partnerName} is my husband`
-            : 'I am not married'}
-        </div>
-      )}
-      {!person.sex ||
-        (person.sex === null && (
-          <div className="Person__partner">`I am not married`</div>
-        ))}
+      <div className="Person__name">My name is {name}</div>
+      {age && age !== null && <div className="Person__age">I am {age}</div>}
+      <div className="Person__partner">{personStatus}</div>
     </div>
   );
 };
